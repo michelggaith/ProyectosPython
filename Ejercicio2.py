83c690ca9bc4a139bfe711a6d7d53c8cdd11db92
Michel Gaith <michelggaith@gmail.com>
2023-04-01 10:31:45 -0300

    Corrección del ejercicio propuesto

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -13,7 +13,7 @@ class Persona():
         print(f'Su DNI es {DNI}  y nacionalidad es {nacionalidad}')
     def __str__(self):
         return f'{self.nombre} edad {self.edad} DNI {self.DNI} nacionalidad {self.nacionalidad}'
-        
+
 persona1 = Persona('Michel', 25, 'Argentina', 40963813)
 persona1.NombreyEdad()
 persona1.devuelve_edad()
